2d32276d06d8f7772d044619b10deffe76fe2330
이지안 <jianlee@ijian-ui-MacBookAir.local>
2025-09-10 19:13:58 +0900

    small fix

--- src/lib/authored.py
+++ src/lib/authored.py
@@ -1,19 +1,14 @@
-### This file is an example of authored script for a micro intervention flow.
-# Later the code will be sent from blockly authoring tool
+### Auto-generated authored script
+def Stay_Present(emotion):
+    yield 'five_senses'
+    if emotion() == 'negative':
+        yield 'notice_five_things'
 
 
-# Example Micro Intervention Set named "Stay Present"
-def stay_present(emotion):
-    yield "notice_five_things"
-    if emotion() == "negative":
-        yield "hands_as_thoughts"
-        if emotion() == "negative":
-            yield "five_senses"
-
-
-# Main function of the script
 def script(emotion):
-    print("[Script] Starting the script with emotion analysis.")
-    if emotion() == "negative":
-        yield from stay_present(emotion)
-    yield "dandelion"
+    print('[Script] Starting the script with emotion analysis.')
+    yield 'five_senses'
+    if emotion() == 'negative':
+        yield from Stay_Present(emotion)
+    else:
+        yield 'dandelion'
